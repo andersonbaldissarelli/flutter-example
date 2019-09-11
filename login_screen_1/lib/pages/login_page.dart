a83921c6490b5b956e6a1f4b3308df3946369a7a
Isadora Oliveira <oliveiraisadora119@gmail.com>
2019-09-11 15:51:06 -0300

    adding new dart file

--- login_screen_1/lib/pages/login_page.dart
+++ login_screen_1/lib/pages/login_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:login_screen/pages/reset-password.page.dart';
+import 'package:login_screen/pages/reset-password_page.dart';
 
 class LoginPage extends StatelessWidget {
   @override
