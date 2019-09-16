df89dfee2416ecfd4e89407f8e95314b70616232
Isadora Oliveira <oliveiraisadora119@gmail.com>
2019-09-16 11:10:13 -0300

    adding new logo image and boxdecoration

--- shoes_store_1/lib/main.dart
+++ shoes_store_1/lib/main.dart
@@ -15,13 +15,29 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: new Column(
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(top: 30.0, bottom: 25.0),
-            //child: Image.asset(),
-          )
-        ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 50.0, vertical: 30.0),
+        child: new Column(
+          children: <Widget>[
+            Align(
+              alignment: Alignment.center,
+              child: Padding(
+                padding: EdgeInsets.only(top: 30.0, bottom: 25.0),
+                child:
+                    Image.asset("assets/logo.png", width: 72.0, height: 53.0),
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              height: 320.0,
+              decoration: BoxDecoration(
+                  color: Color(0xFFfaecfb),
+                  borderRadius: BorderRadius.circular(20.00),
+                  border: Border.all(
+                      color: Colors.grey.withOpacity(.3), width: .2)),
+            )
+          ],
+        ),
       ),
     );
   }
