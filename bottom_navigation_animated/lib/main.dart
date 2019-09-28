c7683395d4deedbaedd021b9b96763a687397da5
Isadora Oliveira <oliveiraisadora119@gmail.com>
2019-09-28 11:17:30 -0300

    update file main.dart

--- bottom_navigation_animated/lib/main.dart
+++ bottom_navigation_animated/lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'bottom_nav_bar.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: new Menu(),
+    );
   }
 }
