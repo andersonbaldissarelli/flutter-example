012cb10b9fcd486d57fdcb8b3a8f59b2e08d46a3
Isadora Oliveira <oliveiraisadora119@gmail.com>
2019-09-29 20:37:25 -0300

    adding new file navigation_drawer.dart

---
+++ navigation_drawer/lib/navigation_drawer.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class NavigationDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
