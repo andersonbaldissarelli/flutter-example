ef848922c22669a266e3a4fb7b1bc15a7c690bce
Isadora Oliveira <oliveiraisadora119@gmail.com>
2019-09-29 22:23:19 -0300

    create file map.dart

---
+++ navigation_drawer/lib/ui/map.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Map extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Mapa"),
+      ),
+      body: Center(
+        child: Text("Mapa"),
+      ),
+    );
+  }
+}
