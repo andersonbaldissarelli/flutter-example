ad98c82d65200ebfeee88019010db04b7143a982
Isadora Oliveira <oliveiraisadora119@gmail.com>
2019-09-29 22:20:44 -0300

    create new file profile.dart

---
+++ navigation_drawer/lib/ui/profile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Profile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Perfil"),
+      ),
+      body: Center(
+        child: Text("Perfil"),
+      ),
+    );
+  }
+}
