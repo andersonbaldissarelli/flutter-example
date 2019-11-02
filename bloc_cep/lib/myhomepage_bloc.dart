a78ffaea2ee8f76597287c08a181466bb2d5e289
Isadora Oliveira <oliveiraisadora119@gmail.com>
2019-11-01 21:57:38 -0300

    define type for EnderecoModel

--- bloc_cep/lib/myhomepage_bloc.dart
+++ bloc_cep/lib/myhomepage_bloc.dart
@@ -1,17 +1,18 @@
 import 'dart:async';
 
+import 'package:bloc_cep/endereco_model.dart';
 import 'package:dio/dio.dart';
 
 class MyHomePageBloc {
   final StreamController<String> _streamController = StreamController();
   Sink<String> get input => _streamController.sink;
-  Stream get output =>
+  Stream<EnderecoModel> get output =>
       _streamController.stream.asyncMap((cep) => _searchCep(cep));
 
   String url(String cep) => "https://viacep.com.br/ws/$cep/json/";
 
-  _searchCep(String cep) async {
+  Future<EnderecoModel> _searchCep(String cep) async {
     Response response = await Dio().get(url(cep));
-    return response.data;
+    return EnderecoModel.fromJson(response.data);
   }
 }
