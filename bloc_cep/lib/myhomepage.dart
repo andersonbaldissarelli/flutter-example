3dd7a20402fa7d7279b76e8e3a6169ebd103168c
Isadora Oliveira <oliveiraisadora119@gmail.com>
2019-11-01 21:29:44 -0300

    add dio in dependencies

--- bloc_cep/lib/myhomepage.dart
+++ bloc_cep/lib/myhomepage.dart
@@ -1,1 +1,37 @@
+import 'package:bloc_cep/myhomepage_bloc.dart';
+import 'package:flutter/material.dart';
 
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  MyHomePageBloc bloc = MyHomePageBloc();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Bloc Example"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            TextField(
+              onChanged: (value) {
+                bloc.input.add(value);
+              },
+              decoration: InputDecoration(
+                hintText: "Digite seu CEP",
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
