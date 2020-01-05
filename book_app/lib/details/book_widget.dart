43aeb8dbb033f4ded0dd127f61b14c7199f183b5
Isadora Oliveira <oliveiraisadora119@gmail.com>
2020-01-04 21:38:21 -0300

    move book_widget of folder

---
+++ book_app/lib/details/book_widget.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class BookWidget extends StatelessWidget {
+  String imageUrl =
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTAN4oXfqMlImBJe4kZiEpYrnaVCA8r_M4zyfsPOvnS-k8KuGjU";
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200,
+      height: 250,
+      margin: EdgeInsets.fromLTRB(16, 12, 16, 12),
+      child: Stack(
+        children: <Widget>[
+          Align(
+            alignment: Alignment.centerRight,
+            child: ClipRRect(
+              borderRadius: BorderRadius.all(
+                Radius.circular(12),
+              ),
+              child: Image.network(
+                imageUrl,
+                fit: BoxFit.fitWidth,
+                width: 200,
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Container(
+              width: 220,
+              height: 200,
+              padding: EdgeInsets.all(24),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(16),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black12,
+                    blurRadius: 15,
+                  ),
+                ],
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    "Torta de Limão",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    "por Isadora Oliveira",
+                    style: TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: Icon(
+                      Icons.arrow_forward_ios,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
