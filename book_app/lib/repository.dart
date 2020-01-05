c3317ce30b0071985a868dccd94e6adc1d078d1f
Isadora Oliveira <oliveiraisadora119@gmail.com>
2020-01-05 12:35:47 -0300

    adding repository for conection api

---
+++ book_app/lib/repository.dart
@@ -0,0 +1,30 @@
+import 'package:book_app/home/bloc/HomePageState.dart';
+import 'package:book_app/models/book_api.dart';
+
+import 'package:http/http.dart' as http;
+
+import 'dart:convert' as converter;
+
+class Repository {
+  //getBooks return state initial page
+
+  final String baseUrl = "https://www.googleapis.com/books/v1/volumes";
+
+  Future<HomePageState> getBooks(String query) async {
+    final url = "$baseUrl?q=$query";
+    var response = await http.get(url);
+
+    //check api conection
+    if (response.statusCode == 200) {
+      var body = response.body;
+      var json = converter.jsonDecode(body);
+      var data = BooksApiModel.fromJson(json);
+
+      var books = data.items;
+
+      return HomePageStateSucess(books: books);
+    } else {
+      return HomePageStateError(message: "Internet Error!");
+    }
+  }
+}
